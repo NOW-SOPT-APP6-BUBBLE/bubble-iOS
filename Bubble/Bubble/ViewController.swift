99452e7101a7398bed50bee096b2dde4ec77ba92
EunsuSeo01 <eunsu0613@naver.com>
2024-05-09 18:40:15 +0900

    Chore: 프로젝트 생성 및 gitignore 적용 #1

---
+++ Bubble/Bubble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bubble
+//
+//  Created by 서은수 on 5/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
