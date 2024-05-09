708fa7301967ddf6e945c4cf0e5a58d66b0b81e0
EunsuSeo01 <eunsu0613@naver.com>
2024-05-10 05:14:10 +0900

    Chore: Logger 추가 #1

--- Bubble/Bubble/Application/SceneDelegate.swift
+++ Bubble/Bubble/Application/SceneDelegate.swift
@@ -12,12 +12,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         
         let navigationController = UINavigationController(rootViewController: HomeViewController())
-        navigationController.navigationBar.isHidden = true
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
     }
--- Bubble/Bubble/Presentation/Home/HomeViewController.swift
+++ Bubble/Bubble/Presentation/Home/HomeViewController.swift
@@ -11,25 +11,23 @@ class HomeViewController: BaseViewController {
     
     // MARK: - Property
     
-    
     // MARK: - Component
-    
     
     // MARK: - Life Cycle
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Logger.debugDescription("로그 예시")
+    }
     
     // MARK: - Set UI
     
-    
     // MARK: - Helper
     
-    
     // MARK: - Action
-    
-    
+        
     // MARK: - Extension
     
-    
     // MARK: - Delegate
-    
 }
