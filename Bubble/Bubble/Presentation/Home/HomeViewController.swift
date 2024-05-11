5f8e9e3ab73491f8d2198eee25e455b9bf5c4965
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-05-12 03:56:26 +0900

    Chore: HomeViewController 로그 삭제 #4

--- Bubble/Bubble/Presentation/Home/HomeViewController.swift
+++ Bubble/Bubble/Presentation/Home/HomeViewController.swift
@@ -17,23 +17,6 @@ class HomeViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        for family in UIFont.familyNames.sorted() {
-            let names = UIFont.fontNames(forFamilyName: family)
-            // Logger를 사용하여 로깅
-            Logger.debugDescription("Family: \(family) Font names: \(names)")
-        }
-        let button = UIButton(type: .system)
-        button.frame = CGRect(x: 50, y: 100, width: 200, height: 50)
-        let attributedText = UILabel.createAttributedText(for: .headline1, withText: "headline1")!
-        button.setAttributedTitle(attributedText, for: .normal)
-        view.addSubview(button)
-        
-        let label = UILabel()
-        label.frame = CGRect(x: 50, y: 200, width: 300, height: 50)
-        label.textAlignment = .center
-        label.attributedText = UILabel.createAttributedText(for: .headline2, withText: "headline2")
-        view.addSubview(label)
     }
 
     // MARK: - Set UI
