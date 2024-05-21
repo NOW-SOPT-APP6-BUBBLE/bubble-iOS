0cd5946c5545df27da8168eed39e584c7ddc2b1d
shimseohyun <shim020924@gmail.com>
2024-05-22 02:15:49 +0900

    Feat: StoreDetail TargetType 추가

--- Bubble/Bubble/Network/Service/ArtistsServeice.swift
+++ Bubble/Bubble/Network/Service/ArtistsServeice.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+import Moya
+
+final class ArtistsServeice {
+    static let shared = ArtistsServeice()
+    private var tempProvider = MoyaProvider<TempTargetType>(plugins: [MoyaLoggingPlugin()])
+    
+    private init() {}
+}
+
+extension ArtistsServeice {
+    func getExample(number: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+        tempProvider.request(.getExample(number: number)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let networkResult = self.judgeStatus(by: statusCode, data, EmptyResultModel.self)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+    
+    private func judgeStatus<T: Codable>(by statusCode: Int, _ data: Data, _ object: T.Type) -> NetworkResult<Any> {
+        switch statusCode {
+        case 200..<205:
+            return isValidData(data: data, T.self)
+        case 400..<500:
+            return .requestError
+        case 500:
+            return .serverError
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func isValidData<T: Codable>(data: Data, _ object: T.Type) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(T.self, from: data) else {
+            print("⛔️ \(self)에서 디코딩 오류가 발생했습니다 ⛔️")
+            return .pathError
+        }
+        return .success(decodedData as Any)
+    }
+}
+
+
--- Bubble/Bubble/Network/TargetType/ArtistsTargetType.swift
+++ Bubble/Bubble/Network/TargetType/ArtistsTargetType.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+import Moya
+
+enum ArtistsTargetType {
+    case getStoreDetail(memberId: String ,artistId: Int)
+}
+
+extension ArtistsTargetType: TargetType {
+    var baseURL: URL {
+        return URL(string: Secret.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getStoreDetail(let memberId, let artistId):
+            return "/api/v1/artists/\(artistId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getStoreDetail(artistId: let artistId):
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getStoreDetail:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        switch self {
+        case .getStoreDetail(memberId: let memberId, artistId: let artistId):
+            return ["Content-Type": "application/json","memberId": memberId]
+        }
+    }
+}
